902adfb680f0fd14b95687b09fec4d01fcfeabd5
Ved Joshi <ved.joshi@gmail.com>
2021-08-10 09:32:10 -0700

    Math Helper class added in C for C practice

---
+++ RetirementCalculator/Other Code FIles/MathHelper.c
@@ -0,0 +1,16 @@
+//
+//  MathHelper.c
+//  RetirementCalculator
+//
+//  Created by Ved Joshi on 8/10/21.
+//
+
+#include "MathHelper.h"
+
+int main(){
+    
+    printf("hi!");
+    return 0;
+    
+    
+}
---
+++ RetirementCalculator/Other Code FIles/MathHelper.h
@@ -0,0 +1,13 @@
+//
+//  MathHelper.h
+//  RetirementCalculator
+//
+//  Created by Ved Joshi on 8/10/21.
+//
+
+#ifndef MathHelper_h
+#define MathHelper_h
+
+#include <stdio.h>
+
+#endif /* MathHelper_h */
